48e3320ffaca2dc051c9475eb181d30dde3b22cd
michael from semaverse dev <mail@semaverse.com>
2025-10-01 18:29:52 +0200

    get rid of old imports

--- servicelayer/{{cookiecutter.project_name}}/app/gen/domainmodel/router.py
+++ servicelayer/{{cookiecutter.project_name}}/app/gen/domainmodel/router.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel
 from app.gen.domainmodel.aiapp import AbstractAiApp
-from app.gen.domainmodel.modelregistry import  BaseModelregistry
 
 class AbstractRouter(BaseModel):
 
